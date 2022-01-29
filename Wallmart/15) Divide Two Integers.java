bfcc9932c4194f09bfbd752fd06d5d05ec3cca56
Tush1810 <tusharpruth@gmail.com>
2022-01-29 16:24:54 +0530

    90th problem solved and challenge completed git add .!

--- Wallmart/15) Divide Two Integers.java
+++ Wallmart/15) Divide Two Integers.java
@@ -8,10 +8,10 @@ class Solution {
         boolean isNeg = ((dividend < 0) ^ (divisor < 0));
         long a = dividend, b = divisor;
         if (dividend < 0) {
-            a = a * -1l;
+            a = -a;
         }
         if (divisor < 0) {
-            b = b * -1l;
+            b = -b;
         }
         for (int i = 31; i >= 0; i--) {
             if ((b << i) <= a) {
@@ -20,8 +20,8 @@ class Solution {
             }
         }
         if (isNeg) ans = -ans;
-        if (ans <= ((1l << 31) * (-1l))) {
-            return (int)((1l << 31) * (-1l));
+        if (ans <= ((-1l << 31))) {
+            return (int)((-1l << 31));
         }
         if (ans >= ((1l << 31) - 1)) {
             return (int)((1l << 31) - 1);
